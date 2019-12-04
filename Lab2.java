9a78ff5fd90a6cb1e06aa0b50a3cb667bf712d6b
Valery Proshchenkov <valery@proshchenkov.ru>
2019-12-05 00:10:43 +0300

    Update Lab2.java

--- Lab2.java
+++ Lab2.java
@@ -4,44 +4,44 @@ import java.util.Scanner;
 
 public class Lab2 {
     private static String shift_encrypt(String text, int shift) {
-        StringBuilder s = new StringBuilder();
+        String str = "";
         for (int i = 0; i < text.length(); i++) {
             if (alphabet.indexOf(text.charAt(i)) + shift + 1 > alphabet.length())
-                s.append(alphabet.charAt(alphabet.indexOf(text.charAt(i)) - (alphabet.length() - shift)));
+                str += alphabet.charAt(alphabet.indexOf(text.charAt(i)) - (alphabet.length() - shift));
             else
-                s.append(alphabet.charAt(alphabet.indexOf(text.charAt(i)) + shift));
+                str += alphabet.charAt(alphabet.indexOf(text.charAt(i)) + shift);
         }
-        return s.toString();
+        return str;
     }
 
     private static String shift_decrypt(String text, int shift) {
-        StringBuilder s = new StringBuilder();
+        String str = "";
         for (int i = 0; i < text.length(); i++) {
             if (alphabet.indexOf(text.charAt(i)) + shift + 1 > alphabet.length())
-                s.append(alphabet.charAt(alphabet.indexOf(text.charAt(i))));
+                str += alphabet.charAt(alphabet.indexOf(text.charAt(i)));
             else
-                s.append(alphabet.charAt(alphabet.indexOf(text.charAt(i))));
+                str += alphabet.charAt(alphabet.indexOf(text.charAt(i)));
         }
-        return s.toString();
+        return str;
     }
 
     private static String xor(String text) {
-        StringBuilder s = new StringBuilder();
+        String str = "";
         String pass = "СЕКРЕТ";
-        int key = 0;
-        for (int i = 0; i < pass.length(); i++) key += pass.charAt(i);
-        for (int i = 0; i < text.length(); i++) s.append((char) ((text.charAt(i)) ^ key));
-        return s.toString();
+        for (int i = 0; i < text.length(); i++) str += (char) ((text.charAt(i)) ^ pass.charAt(i % 6));
+        return str;
     }
 
     private static String alphabet;
 
     public static void main(String[] args) {
         int menu, shift;
-        Scanner scanner = new Scanner(System.in);
         String text;
-        alphabet = " !,.0123456789?";
+        alphabet = " !,-.0123456789?";
+        Scanner scanner = new Scanner(System.in);
         for (int i = 1040; i < 1104; i++) alphabet += (char) i;
+        for (int i = 65; i < 91; i++) alphabet += (char) i;
+        for (int i = 97; i < 123; i++) alphabet += (char) i;
         while (true) {
             System.out.println("[1] - Циклическая замена");
             System.out.println("[2] - Исключающее или");
@@ -49,8 +49,10 @@ public class Lab2 {
             menu = scanner.nextInt();
             if (menu == 1) {
                 System.out.println("Введите текст:");
-                text = scanner.next();
+                scanner = new Scanner(System.in);
+                text = scanner.nextLine();
                 System.out.println("Введите сдвиг:");
+                scanner = new Scanner(System.in);
                 shift = scanner.nextInt();
                 shift %= alphabet.length();
                 System.out.println("Зашифрованный текст:");
@@ -59,7 +61,8 @@ public class Lab2 {
                 System.out.println(shift_decrypt(text, shift));
             } else if (menu == 2) {
                 System.out.println("Введите текст:");
-                text = scanner.next();
+                scanner = new Scanner(System.in);
+                text = scanner.nextLine();
                 System.out.println("Зашифрованный текст:");
                 System.out.println(xor(text));
                 System.out.println("Расшифрованный текст:");
